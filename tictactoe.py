e082e66362dd9b85136397f9f45d2c03f6312010
Vedant Walia <vedantwalia@outlook.com>
2024-10-20 17:32:45 +0530

    logic testing update

--- tictactoe.py
+++ tictactoe.py
@@ -2,3 +2,18 @@ import numpy as np
 import pygame
 import math
 
+ROWS = 3
+COLUMNS = 3
+
+def mark(row, col, player):
+    board[row][col] = player
+
+def is_valid_mark(row, col):
+    return board[row][col] == 0
+
+board = np.zeros((ROWS, COLUMNS))
+
+print(board)
+mark(2,1,2)
+print(board)
+print(is_valid_mark(2,1))
