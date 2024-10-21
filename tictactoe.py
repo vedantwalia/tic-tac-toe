c78913e574668d75fc8fbe3665f4fcf50389532c
Vedant Walia <vedantwalia@outlook.com>
2024-10-21 19:15:01 +0530

    update gitignore and code

--- tictactoe.py
+++ tictactoe.py
@@ -5,6 +5,12 @@ import math
 ROWS = 3
 COLUMNS = 3
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+WIDTH = 600
+HEIGHT = 600
+SIZE = (WIDTH, HEIGHT)
+
 def mark(row, col, player):
     board[row][col] = player
 
@@ -17,6 +23,12 @@ def is_board_full():
             if board[r][c] == 0:
                 return False
     return True
+
+def draw_lines():
+    pygame.draw.line(window, BLACK, (200,0), (200, 600), 10)
+    pygame.draw.line(window, BLACK, (400,0), (400, 600), 10)
+    pygame.draw.line(window, BLACK, (0,200), (200, 600), 10)
+    pygame.draw.line(window, BLACK, (0,400), (600, 400), 10)
 
 def is_winning_move(player):
     if player == 1:
@@ -45,7 +57,15 @@ game_over = False
 
 Turn = 0
 
-while not game_over:
+pygame.init() #initialize the game window
+window = pygame.display.set_mode(SIZE)
+pygame.display.set_caption('tic tac toe')
+window.fill(WHITE)
+draw_lines()
+pygame.display.update()
+pygame.time.wait(2000)
+
+""" while not game_over:
     if Turn % 2 == 0:
         #Player 1 turn
         row = int(input("Player 1: Choose row number (0-2): "))
@@ -73,3 +93,7 @@ while not game_over:
 
     if game_over == True:
         print("Game Over!!!")
+
+
+ """
+
